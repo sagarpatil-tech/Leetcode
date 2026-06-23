893db2985cc5481d5eec2d4903c69c507fba18b0
Sagar Patil <0176cs231172@lnct.ac.in>
2026-06-24 02:46:32 +0530

    Time: 2 ms (98.74%), Space: 43.9 MB (62.32%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder str = new StringBuilder();
+        int i = num1.length()-1;
+        int j = num2.length()-1;
+        int carry =0;
+        while(i>=0 || j>=0 || carry!= 0){
+             int x = (i >= 0)? num1.charAt(i) - '0':0;
+             int y = (j >= 0)? num2.charAt(j) - '0':0;
+
+            int sum = x + y + carry;
+            carry = sum /10;
+             int digit = sum%10;
+            str.append(digit);
+            i--;
+            j--;
+        }
+        str.reverse();
+        return str.toString();
+       
+    }
+}
