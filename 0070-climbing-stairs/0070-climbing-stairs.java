148449a0ab0cd3b889182770c5e7a2cbd0239cc9
Sagar Patil <0176cs231172@lnct.ac.in>
2026-08-09 11:11:10 +0530

    Time: 0 ms (100%), Space: 41.9 MB (87.57%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+       int[] dp = new int[n+1];
+       return solve(n , dp);
+    }
+    public int solve(int n , int []dp){
+          if(n<=2){
+            return n;
+          }
+          if(dp[n] != 0){
+            return dp[n];
+
+          }
+          dp[n] = solve(n-1 , dp) + solve(n-2, dp);
+          return dp[n];
+    }
+}
