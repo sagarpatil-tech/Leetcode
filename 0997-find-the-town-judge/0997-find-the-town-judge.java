10ec30ec7403647f40313b077f51774010c25d01
Sagar Patil <0176cs231172@lnct.ac.in>
2026-09-26 23:12:08 +0530

    Time: 2 ms (99.76%), Space: 53.1 MB (95.87%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+       int[] degree = new int[n + 1];
+       for(int[] t: trust){
+        degree[t[0]]--;
+        degree[t[1]]++;
+       }
+       for(int i=1; i<=n; i++){
+        if(degree[i] == n-1){
+            return i;
+        }
+       }
+       return -1;
+    }
+}
